46ec7ffbcefce1b8d7dec504727dc17262f37c96
arko617 <arkox617@gmail.com>
2017-09-17 17:00:46 -0700

    done

---
+++ docs/js/main.js
@@ -0,0 +1,16 @@
+// Changes text/color based on user input
+
+var getInput = function(){
+	var answer = document.getElementById("textInput").value;
+	if(answer === '2'){
+		document.getElementById("notification").style.backgroundColor = "green";
+		document.getElementById("notificationHeader").innerHTML = "Success";
+		document.getElementById("notificationText").innerHTML = "Correct Answer";
+	}
+
+	else{
+		document.getElementById("notification").style.backgroundColor = "red";
+		document.getElementById("notificationHeader").innerHTML = "Error";
+		document.getElementById("notificationText").innerHTML = "Incorrect Input";
+	}
+}
